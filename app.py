b19542ac4129914f1d6b09078353fc7cffeef996
Luis Salazar <l4sh.dev@gmail.com>
2017-09-19 00:06:00 -0400

    Add serve client from flask

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import logging
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='client/dist/static',
+    template_folder='client/dist')
+
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @socketio.on('message')
 def handle_message(json):
