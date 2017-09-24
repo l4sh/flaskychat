baa0dc9baf87ad0eb13b49112425af7c709aa043
Luis Salazar <l4sh.dev@gmail.com>
2017-09-24 12:05:35 -0400

    Add Chat component as main

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Chat from '@/components/Chat'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/chat',
       name: 'Chat',
       component: Chat
     }
